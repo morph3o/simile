7a3163121ed6b1ece81532623721d207afe8ebb1
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2017-01-17 03:50:45 -0300

    Explore test package

--- src/main/java/de/uni/mannheim/simile/ClonerMain.java
+++ src/main/java/de/uni/mannheim/simile/ClonerMain.java
@@ -1,5 +1,6 @@
 package de.uni.mannheim.simile;
 
+import com.google.common.base.Strings;
 import de.uni.mannheim.simile.services.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,20 +14,28 @@ public class ClonerMain {
 
 	private static final String REPO = "https://github.com/morph3o/simile.git";
 	private static final String BRANCH = "master";
-	private static final String FOLDER = "tmp";
+	private static final String FOLDER = "project";
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Cloner cloner = new Cloner(REPO, BRANCH, FOLDER);
 
 		cloner.cloneRepository();
 
-		File projectDir = new File(String.format("%s/src", FOLDER));
+		File maintDir = new File(String.format("%s/src/main", FOLDER));
 		JavaClassHandler jch = new JavaClassHandler();
-		DirectoryExplorer dirExplorer = new DirectoryExplorer(jch, new JavaClassFilter());
-		dirExplorer.explore(projectDir);
+		new DirectoryExplorer(jch, new JavaClassFilter()).explore(maintDir);
 
-		System.out.println(jch.getMethods().get(0));
-		SocoraRequester.searchComponent(jch.getMethods().get(0));
+		File testDir = new File(String.format("%s/src/test", FOLDER));
+		JavaClassHandler jch2 = new JavaClassHandler();
+		new DirectoryExplorer(jch2, new JavaClassFilter()).explore(testDir);
+
+		System.out.println(String.format("Methods found in project: %s", jch.getMethods().size()));
+		System.out.println(String.format("Test classes found in project: %s", jch2.getTestClasses().size()));
+
+		System.out.println(String.format("Test class to search"));
+		System.out.println(Strings.repeat("=", "Test class to search".length()));
+		System.out.println(jch2.getTestClasses().get(1));
+		new SocoraRequester().searchComponent(jch2.getTestClasses().get(1), SocoraRequester.TEST_DRIVEN_SEARCH);
 	}
 
 }
