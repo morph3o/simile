f4d1a3320756307874af3bb6196c45fd69402b6e
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2017-01-17 03:50:18 -0300

    Separate searchComponent method in two
    We separated the searchComponent method in two specifying the type of search we want to make. There three possible search types, test-driven search, interface-driven search and keyword search. The last two are the same in term of code.

--- src/main/java/de/uni/mannheim/simile/services/SocoraRequester.java
+++ src/main/java/de/uni/mannheim/simile/services/SocoraRequester.java
@@ -18,6 +18,9 @@ import java.util.List;
 import java.util.Map;
 
 public class SocoraRequester {
+	public static String TEST_DRIVEN_SEARCH = "TEST_DRIVEN_SEARCH";
+	public static String INTERFACE_DRIVEN_SEARCH = "INTERFACE_DRIVEN_SEARCH";
+	public static String KEYWORD_SEARCH = "KEYWORD_SEARCH";
 
 	private static final Logger LOG = LoggerFactory.getLogger(SocoraRequester.class);
 
@@ -26,7 +29,76 @@ public class SocoraRequester {
 	private static String user = "socora";
 	private static String pass = "d3fudj983r223dhs23";
 
-	public static void searchComponent(String method) throws IOException {
+	public void searchComponent(String query, String searchType) throws IOException, InterruptedException {
+		Validate.notBlank(query, "Query parameter is required and cannot be blank");
+		if (StringUtils.compare(searchType, INTERFACE_DRIVEN_SEARCH) == 0 ||
+			StringUtils.compare(searchType, KEYWORD_SEARCH) == 0 ||
+			StringUtils.isBlank(searchType)) {
+			this.textualSearchComponent(query);
+		} else if (StringUtils.compare(searchType, TEST_DRIVEN_SEARCH) == 0) {
+			this.testDrivenSearchComponent(query);
+		} else {
+			this.textualSearchComponent(query);
+		}
+	}
+
+	private void testDrivenSearchComponent(String testClass) throws IOException, InterruptedException {
+		// create client
+		CandidateSearchClient candidateSearchClient = new CandidateSearchClient(baseURI, auth(user, pass));
+
+		// search request
+		CandidateSearchRequest request = new CandidateSearchRequest();
+
+		// maximum no. of candidates to retrieve
+		int maxResults = 10;
+
+		//
+		String testClassSourceQuery = testClass;
+
+		request.setInput(testClassSourceQuery);
+		QueryParams queryParams = new QueryParams();
+		queryParams.setRows(maxResults);
+
+		// inclusions
+		queryParams.setArtifactInformation(true/* maven metadata */);
+		queryParams.setContent(true/* source code */);
+
+		// FILTERS
+		queryParams.setFilters(Arrays.asList(Filters.HASH_CODE_CLONE_FILTER, Filters.NO_ABSTRACT_CLASS_FILTER,
+			Filters.NO_INTERFACE_FILTER, Filters.LATEST_VERSION_FILTER, Filters.FUNCTIONAL_SUFFICIENCY_FILTER));
+		request.setQueryParams(queryParams);
+		// MUST be set to true for test-driven search
+		request.setTestDrivenSearch(Boolean.TRUE);
+
+		// set ranking
+		queryParams.setRankingStrategy(Rankings.SINGLE_OBJECTIVE);
+
+		// set ranking criteria
+		List<RankingCriterion> rankingCriteria = Arrays.asList(
+			// textual score (Lucene/SolR)
+			RankingCriterionBuilder.rankingCriterion().withName("luceneScore").withObjective(RankingCriterion.MAX)
+				.build());
+
+		queryParams.setRankingCriteria(rankingCriteria);
+
+		CandidateSearchResponse response = candidateSearchClient.search(request);
+
+		// response, here the returned jobId of the TDS job is important!
+		String jobId = response.getJobId();
+
+		// now TDS is running, poll job status, wait until finished (NOTE: do
+		// something useful here)
+		JobStatus jobStatus = null;
+		while (jobStatus == null || jobStatus.getStatusType() == JobStatusType.RUNNING) {
+			//
+			jobStatus = candidateSearchClient.getJobStatus(jobId);
+
+			// sleep
+			Thread.sleep(30 * 1000L);
+		}
+	}
+
+	private void textualSearchComponent(String method) throws IOException {
 		// create client
 		CandidateSearchClient candidateSearchClient = new CandidateSearchClient(baseURI, auth(user, pass));
 
