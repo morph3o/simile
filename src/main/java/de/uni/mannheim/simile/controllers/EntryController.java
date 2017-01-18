f7177148512834b3553d06b3117555118708cfac
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2017-01-18 08:08:22 -0300

    Added git url and email validators

--- src/main/java/de/uni/mannheim/simile/controllers/EntryController.java
+++ src/main/java/de/uni/mannheim/simile/controllers/EntryController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @RestController
 public class EntryController {
@@ -31,7 +32,7 @@ public class EntryController {
 			return ResponseEntity.badRequest().body(validateRequest(repo, email).get());
 		} else {
 			LOG.debug("The request is valid. Request -> repo: %s - branch: %s - email: %s", repo, branch, email);
-			simile.searchForComponents(repo, branch, "projectDir");
+			//simile.searchForComponents(repo, branch, "projectDir");
 			return ResponseEntity.ok(new Message("Repository set successfully", 200));
 		}
 	}
@@ -39,12 +40,38 @@ public class EntryController {
 	private Optional<Message> validateRequest(String repo, String email) {
 		LOG.debug("Validating required parameters in request");
 		StringBuilder str = new StringBuilder();
-		if(repo.isEmpty()) str.append("Repo is required");
+		if(repo.isEmpty()) str.append("repo is required");
 		if(email.isEmpty()) str.append("email is required");
+		if(!this.isValidEmail(email)) str.append("email is not valid");
+		if(!this.isValidGithubWebURL(repo)) str.append("repo must be a valid Git Web URL (e.g. https://github.com/...)");
 		if(!str.toString().isEmpty()) {
 			return Optional.ofNullable(new Message(str.toString(), 400));
 		}
 		return Optional.empty();
+	}
+
+	/**
+	 * Validates if a Git repository url is a valid Git Web URL.
+	 *
+	 * @param gitRepo Git web URL to be validated.
+	 * @return true if the git url is valid, otherwise false.
+	 * */
+	private boolean isValidGithubWebURL(String gitRepo) {
+		final Pattern pattern = Pattern.compile("(http(s)?)(:(//)?)([\\w\\.@\\:/\\-~]+)(\\.git)?");
+		return pattern.matcher(gitRepo).matches();
+	}
+
+	/**
+	 * Validates if an email address is valid.
+	 *
+	 * @see <a href="http://www.regexr.com/3c0ol">http://www.regexr.com/3c0ol</a>
+	 *
+	 * @param email email address to be validated.
+	 * @return true in case the email address is valid, otherwise false.
+	 * */
+	private boolean isValidEmail(String email) {
+		final Pattern pattern = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+		return pattern.matcher(email).matches();
 	}
 
 }
--- src/test/java/de/uni/mannheim/simile/controllers/EntryControllerTests.java
+++ src/test/java/de/uni/mannheim/simile/controllers/EntryControllerTests.java
@@ -3,8 +3,9 @@ package de.uni.mannheim.simile.controllers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,19 +14,27 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.Charset;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.hamcrest.core.StringContains.containsString;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EntryControllerTests {
 
+	private static final Logger LOG = LoggerFactory.getLogger(EntryControllerTests.class);
+
 	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
-																														MediaType.APPLICATION_JSON.getType(),
-																														MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+		MediaType.APPLICATION_JSON.getType(),
+		MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+	private static final String VALID_GIT_URL = "https://github.com/morph3o/simile.git";
+	private static final String INVALID_GIT_URL = "invalid github url";
+
+	private static final String VALID_EMAIL = "pdivasto@gmail.com";
+	private static final String INVALID_EMAIL = "invalid email";
 
 	private MockMvc mockMvc;
 
@@ -33,18 +42,19 @@ public class EntryControllerTests {
 	private WebApplicationContext webApplicationContext;
 
 	@Before
-	public void setup(){
+	public void setup() {
 		this.mockMvc = webAppContextSetup(webApplicationContext).build();
 	}
 
 	@Test
 	public void itShouldSetRepositoryWithRepoAndBranch() throws Exception {
 		this.mockMvc.perform(post("/repository")
-														.param("repo", "repo test")
-														.param("branch", "branch test")
-														.param("email", "email test")
-														.contentType(APPLICATION_JSON_UTF8))
-			.andExpect(status().isOk());
+			.param("repo", VALID_GIT_URL)
+			.param("branch", "branch test")
+			.param("email", VALID_EMAIL)
+			.contentType(APPLICATION_JSON_UTF8))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("\"Repository set successfully")));
 	}
 
 	@Test
@@ -54,21 +64,41 @@ public class EntryControllerTests {
 	}
 
 	@Test
-	public void itShouldReturnErrorForEmptyEmail() throws Exception {
+	public void itShouldReturnErrorForEmptyRepo() throws Exception {
 		this.mockMvc.perform(post("/repository")
 			.param("repo", "")
 			.param("branch", "branch test")
-			.param("email", "email test")
+			.param("email", VALID_EMAIL)
 			.contentType(APPLICATION_JSON_UTF8))
 			.andExpect(status().is4xxClientError());
 	}
 
 	@Test
-	public void itShouldReturnErrorForEmptyRepo() throws Exception {
+	public void itShouldReturnErrorForEmptyEmail() throws Exception {
 		this.mockMvc.perform(post("/repository")
-			.param("repo", "repo test")
+			.param("repo", VALID_GIT_URL)
 			.param("branch", "branch test")
 			.param("email", "")
+			.contentType(APPLICATION_JSON_UTF8))
+			.andExpect(status().is4xxClientError());
+	}
+
+	@Test
+	public void itShouldReturnErrorForInvalidRepo() throws Exception {
+		this.mockMvc.perform(post("/repository")
+			.param("repo", INVALID_GIT_URL)
+			.param("branch", "branch test")
+			.param("email", VALID_EMAIL)
+			.contentType(APPLICATION_JSON_UTF8))
+			.andExpect(status().is4xxClientError());
+	}
+
+	@Test
+	public void itShouldReturnErrorForInvalidEmail() throws Exception {
+		this.mockMvc.perform(post("/repository")
+			.param("repo", VALID_GIT_URL)
+			.param("branch", "branch test")
+			.param("email", INVALID_EMAIL)
 			.contentType(APPLICATION_JSON_UTF8))
 			.andExpect(status().is4xxClientError());
 	}
